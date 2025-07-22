41d3e3713a05b101092fdeaf80914c44dfcfe203
Priyabrata Pattanaik <githubprojects100@gmail.com>
2025-07-23 00:16:49 +0530

    Full code added

--- src/main/java/com/playschool/management/entity/VehicleOwner.java
+++ src/main/java/com/playschool/management/entity/VehicleOwner.java
@@ -186,7 +186,37 @@ public class VehicleOwner {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Address {
-        private String street;
+        public String getStreet() {
+			return street;
+		}
+		public void setStreet(String street) {
+			this.street = street;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getPincode() {
+			return pincode;
+		}
+		public void setPincode(String pincode) {
+			this.pincode = pincode;
+		}
+		public String getCountry() {
+			return country;
+		}
+		public void setCountry(String country) {
+			this.country = country;
+		}
+		private String street;
         private String city;
         private String state;
         private String pincode;
--- src/main/java/com/playschool/management/service/CustomerService.java
+++ src/main/java/com/playschool/management/service/CustomerService.java
@@ -1,13 +1,15 @@
 package com.playschool.management.service;
 
-import com.playschool.management.entity.Customer;
 import java.util.List;
-
 import java.util.Optional;
-import org.springframework.http.ResponseEntity;
+
+import com.playschool.management.dto.CustomerResponseDto;
+import com.playschool.management.entity.Customer;
 
 public interface CustomerService {
     List<Customer> getAllCustomers();
     Optional<Customer> getCustomerById(String id);
     Optional<Customer> getCustomerByUserId(String userId);
+    CustomerResponseDto mapToResponseDto(Customer customer);
+
 }
