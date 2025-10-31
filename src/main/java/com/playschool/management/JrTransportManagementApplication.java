bca7557d0b9eaad880f7298c0b82a5463c312b3d
Motuitionapplication <motuitionapplication@gmail.com>
2025-10-31 16:13:52 +0530

    test build error in git 2

--- src/main/java/com/playschool/management/JrTransportManagementApplication.java
+++ src/main/java/com/playschool/management/JrTransportManagementApplication.java
@@ -3,9 +3,14 @@ package com.playschool.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+	    "com.playschool.management", 
+	    "com.playschool.management.security.jwt"
+	})
 @EntityScan("com.playschool.management.entity")
 @EnableJpaRepositories("com.playschool.management.repository")
 public class JrTransportManagementApplication {
